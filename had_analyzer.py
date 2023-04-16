165273db5567057e9abf7588030c42e684c55c36
ai4ki <91087420+ai4ki@users.noreply.github.com>
2023-04-16 15:25:09 +0200

    Update had_analyzer.py

--- had_analyzer.py
+++ had_analyzer.py
@@ -15,7 +15,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.options import Options, Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from typing import List
 
